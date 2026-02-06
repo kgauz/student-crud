962c56630e97fd908f7fe134db52a68bc7741d60
kgauza <125618612+kgauz@users.noreply.github.com>
2026-02-06 14:12:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/students", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
